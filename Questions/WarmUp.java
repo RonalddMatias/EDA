f41a243579eb068dff1e91cecc3ddf7a694e4e49
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-07-28 20:08:15 -0300

    WarmUp

---
+++ Questions/WarmUp.java
@@ -0,0 +1,20 @@
+package EDA.Questions;
+
+import java.util.Scanner;
+
+public class WarmUp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int multiplier = sc.nextInt();
+        sc.nextLine();
+        String[] numbers = sc.nextLine().split(" ");
+
+        String output = "";
+        for(int i = 0; i < numbers.length; i++){
+            output += Integer.toString(Integer.parseInt(numbers[i])*multiplier) + " ";
+        }   
+
+        System.out.println(output.trim());
+    }
+}
