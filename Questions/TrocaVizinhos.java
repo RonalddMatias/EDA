85acb31862c8565c36006d4245b416ef39e2f695
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-07-28 22:59:42 -0300

    TrocaVizinhos

---
+++ Questions/TrocaVizinhos.java
@@ -0,0 +1,22 @@
+package EDA.Questions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// se o tamanho da sequência for impar, mantenha o ultimo elemento.
+public class TrocaVizinhos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] numeros = sc.nextLine().split(" ");
+        
+        for(int i = 0; i < numeros.length -1; i += 2){ // O +2 deve-se ao fato de que eu quero pegar somente dois pares
+            int temp = Integer.parseInt(numeros[i]);
+            numeros[i] = numeros[i+1];
+            numeros[i + 1] = Integer.toString(temp);
+        }
+        
+        System.out.println(Arrays.toString(numeros)); 
+        
+    }
+}
